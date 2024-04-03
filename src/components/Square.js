7313d4f8f93f3f86f1f50c0a5375b526a0d6b82f
zizondazi <ektmf0129@gmail.com>
2024-04-03 15:33:58 +0900

    add files

---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Square.css';
+
+export default class Square extends Component {
+  render() {
+    return (
+      <button className='square' onClick={() => {console.log('click')}}>
+        {this.props.value}
+      </button>
+    )
+  }
+}
