f0a546abe296a18503c2c374b8eaf7c1e365aad7
zizondazi <ektmf0129@gmail.com>
2024-04-09 16:54:21 +0900

    add history

--- src/App.js
+++ src/App.js
@@ -1,13 +1,84 @@
+import { useState } from 'react';
 import './App.css'
 import Board from './components/Board';
 
 function App() {
+  const [history, setHistory] = useState([{ squares : Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+  const [stepNumber, setStepNumber] = useState(0);
+
+  const current = history[stepNumber];
+
+  const calculateWinner = (squares) => {
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+
+    for(let i = 0; i < lines.length; i++) {
+        const [a,b,c] = lines[i];
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }  
+        return null;
+    }
+  }
+  
+  let status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  const winner = calculateWinner(current.squares);
+
+  if(winner) {
+      status = 'Winnner : ' + winner;
+  }
+
+  const handleClick = (i) => {
+    const newHistory = history.slice(0, stepNumber + 1);
+    const newCurrent = newHistory[newHistory.length - 1];
+    const newSquares = newCurrent.squares.slice();
+
+    if(calculateWinner(newSquares) || newSquares[i]) {
+        return;
+    }
+
+    newSquares[i] = xIsNext? "X" : "O";
+    setHistory([...newHistory, { squares : newSquares }]);
+    setXIsNext(prev => !prev);
+
+    setStepNumber(newHistory.length);
+  }
+
+  const moves = history.map((step, move) => {
+    const desc = move ? 'Go to move #' + move : 'Go to game start';
+    
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{desc}</button>
+      </li>
+    )
+  });
+
+  const jumpTo = (step) => {
+    setStepNumber(step);
+    setXIsNext((step % 2) === 0);
+  }
+
   return (
     <div className="game">
         <div className="game-board">
-          <Board />
+          <Board 
+            squares={current.squares}
+            onClick={(i) => handleClick(i)}
+          />
         </div>
         <div className="game-info">
+          <div className='status'>{status}</div>
+          <ol>{moves}</ol>
         </div>
     </div>
   );
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,61 +1,17 @@
-import React, { useState } from 'react'
 import Square from './Square'
 import './Board.css'
 
-const Board = () => {
-    const [squares, setSquares] = useState(Array(9).fill(null));
-    const [xIsNext, setXIsNext] = useState(true);
+const Board = ({ squares, onClick }) => {
 
-    const calculateWinner = (squares) => {
-        const lines = [
-            [0,1,2],
-            [3,4,5],
-            [6,7,8],
-            [0,3,6],
-            [1,4,7],
-            [2,5,8],
-            [0,4,8],
-            [2,4,6]
-        ]
-
-        for(let i = 0; i < lines.length; i++) {
-            const [a,b,c] = lines[i];
-            if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-                return squares[a];
-            }  
-            return null;
-        }
-    }
-
-    const winner = calculateWinner(squares);
-    let status = `Next player: ${xIsNext ? 'X' : 'O'}`;
-
-    if(winner) {
-        status = 'Winnner : ' + winner;
-    }
-    
     const renderSquare = (i) => {
         return <Square 
                     value={squares[i]}
-                    onClick={() => {handleClick(i)}}
+                    onClick={() => {onClick(i)}}
                 />;
-    }
-
-    const handleClick = (i) => {
-        const newSquares = squares.slice();
-
-        if(calculateWinner(newSquares) || newSquares[i]) {
-            return;
-        }
-
-        newSquares[i] = xIsNext? "X" : "O";
-        setSquares(newSquares);
-        setXIsNext(prev => !prev);
     }
 
     return (
         <div>
-            <div className='status'>{status}</div>
             <div className='board-row'>
                 {renderSquare(0)}
                 {renderSquare(1)}
