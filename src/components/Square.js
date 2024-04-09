a4fe4eea75ae6349bc59c4a81d94e63b64cbb6d8
zizondazi <ektmf0129@gmail.com>
2024-04-09 15:31:23 +0900

    click event add

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react'
 import './Square.css';
 
-export default class Square extends Component {
+const Square = ({ value, onClick }) => {
 
-    render() {
-        return (
-        <button className='square' 
-            onClick={() => {this.props.onClick()}}
-        >
-            {this.props.value}
-        </button>
-        )
-    }
+    return (
+    <button className='square' 
+        onClick={onClick}
+    >
+        {value}
+    </button>
+    )
 }
+
+export default Square
